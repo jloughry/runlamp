b0df8b393aeff41de1d92b6137707e94ecec26e3
Joe Loughry <joe.loughry@gmail.com>
2018-04-29 03:53:50 +0000

    abstract GPIO pin numbers to header file

---
+++ gpio_pin_numbers.h
@@ -0,0 +1,12 @@
+// The following are GPIO pin numbers (not physical pins) on J8:
+
+#define RUN           5
+#define TRAFFIC      13
+#define WAIT         25
+#define BLOCKED      26
+#define FAULT        26
+#define PHOTODIODE_A  4
+#define LASER_A       4
+#define PHOTODIODE_B 22
+#define LASER_B      22
+
