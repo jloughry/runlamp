1450ec4b3f159e5e5e49938471aca48dbbe3fb07
Joe Loughry <joe.loughry@gmail.com>
2018-04-28 22:28:49 -0600

    update runlamp to use new header file

--- runlamp.c
+++ runlamp.c
@@ -7,17 +7,10 @@
 #include <sys/types.h>
 #include <err.h>
 #include <libgpio.h>
-
-#define RUN 5
-#define TRAFFIC 13
-#define BLOCKED 26
-#define FAULT 26
-#define PHOTODIODE_A 4
-#define LASER_A 4
-#define PHOTODIODE_B 22
-#define LASER_B 22
+#include "gpio_pin_numbers.h"
 
 #define PERIOD 1000000 /* microseconds */
+@define WHICH_LAMP RUN
 
 int main(void) {
 	gpio_handle_t handle;
@@ -26,11 +19,11 @@ int main(void) {
 	if (GPIO_INVALID_HANDLE == handle) {
 		err(1, "gpio_open() failed");
 	}
-	gpio_pin_output(handle, RUN);
+	gpio_pin_output(handle, WHICH_LAMP);
 	for (;;) {
-		gpio_pin_high(handle, RUN);
+		gpio_pin_high(handle, WHICH_LAMP);
 		usleep(PERIOD);
-		gpio_pin_low(handle, RUN);
+		gpio_pin_low(handle, WHICH_LAMP);
 		usleep(PERIOD);
 	}
 	gpio_close(handle);
